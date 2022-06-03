4422e72804c2dee5403a29457405b564e118fed0
Marvin Zhang <tikazyq@163.com>
2022-06-03 23:10:38 +0800

    updated chromedriver.sh

--- crawlab-base/install/chromedriver.sh
+++ crawlab-base/install/chromedriver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget https://chromedriver.storage.googleapis.com/103.0.5060.24/chromedriver_linux64.zip \
+wget https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_linux64.zip \
 	&& unzip chromedriver_linux64.zip \
 	&& mv chromedriver /usr/local/bin
