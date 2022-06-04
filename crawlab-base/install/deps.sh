f4fe7d27f9373ddc820124b4167dedcf4f3554b3
Marvin Zhang <tikazyq@163.com>
2022-06-04 13:48:41 +0800

    updated chromedriver.sh

--- crawlab-base/install/deps.sh
+++ crawlab-base/install/deps.sh
@@ -2,4 +2,5 @@
 
 chmod 777 /tmp \
 	&& apt-get update \
-	&& apt-get install -y curl git net-tools iputils-ping ntp ntpdate nginx wget dumb-init cloc unzip
+	&& apt-get install -y curl git net-tools iputils-ping ntp ntpdate nginx wget dumb-init cloc unzip # common deps
+	&& apt-get install -y libglib2.0-0 libnss3 libx11-6 # chromedriver deps
