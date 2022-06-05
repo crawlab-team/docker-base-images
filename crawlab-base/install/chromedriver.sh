5a03928d3309c7c5b382cccf8a8844f2f891a102
Marvin Zhang <tikazyq@163.com>
2022-06-05 15:11:59 +0800

    fixed chromedriver issue

--- crawlab-base/install/chromedriver.sh
+++ crawlab-base/install/chromedriver.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# google-chrome
+cd /tmp \
+  && apt-get update \
+  && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
+  && apt-get install -y ./google-chrome-stable_current_amd64.deb && \
+
+# chromedriver
 wget https://chromedriver.storage.googleapis.com/102.0.5005.61/chromedriver_linux64.zip \
 	&& unzip chromedriver_linux64.zip \
 	&& mv chromedriver /usr/local/bin
