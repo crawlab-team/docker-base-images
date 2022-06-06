54ab32c1685f2f2a1a93cc0b7ac7c02b3bdc7572
Marvin Zhang <tikazyq@163.com>
2022-06-06 13:20:34 +0800

    fixed chromedriver issue

--- crawlab-base/install/chromedriver.sh
+++ crawlab-base/install/chromedriver.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-apt-get install -y chromium-chromedriver
-ln -s /usr/lib/chromium-browser/chromedriver /usr/local/bin/chromedriver
+# deps
+apt-get install -y unzip xvfb libxi6 libgconf-2-4
+
+# chrome
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+bash -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google-chrome.list"
+apt-get update
+apt-get -y install google-chrome-stable
+echo `google-chrome --version`
+
+# chromedriver
+wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/local/bin/chromedriver
+chown root:root /usr/local/bin/chromedriver
+chmod +x /usr/local/bin/chromedriver
