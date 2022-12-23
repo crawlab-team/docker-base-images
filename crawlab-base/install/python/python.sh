5fe1f5b0efbb69c4d9b59e4147460b9a9b3b6214
Marvin Zhang <tikazyq@163.com>
2022-12-23 11:52:59 +0800

    feat(env): upgrade to python 3.10

--- crawlab-base/install/python/python.sh
+++ crawlab-base/install/python/python.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 # install python
-apt-get install -y python3 python3-pip \
-	&& ln -s /usr/bin/pip3 /usr/local/bin/pip \
-	&& ln -s /usr/bin/python3 /usr/local/bin/python
+apt-get update \
+	&& apt install software-properties-common -y \
+	&& add-apt-repository ppa:deadsnakes/ppa -y \
+	&& apt install python3.10 -y \
+	&& curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10 \
+ 	&& ln -s /usr/local/bin/pip3.10 /usr/local/bin/pip \
+ 	&& ln -s /usr/bin/python3.10 /usr/local/bin/python
 
 # install python dependencies
 pip install -r /app/install/python/requirements.txt
