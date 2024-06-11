3892520d0e562edcee94bad58c83e01e15668122
Marvin Zhang <tikazyq@163.com>
2024-06-11 17:15:02 +0800

    feat: updated deps

--- crawlab-base/install/deps/deps.sh
+++ crawlab-base/install/deps/deps.sh
@@ -7,7 +7,7 @@ chmod 777 /tmp
 apt-get update
 
 # common deps
-apt-get install -y curl git net-tools iputils-ping ntp ntpdate nginx wget dumb-init cloc unzip build-essential gnupg2
+apt-get install -y curl git net-tools iputils-ping ntp ntpdate nginx wget dumb-init cloc unzip build-essential gnupg2 libc6
 
 # chromedriver deps
 apt-get install -y libglib2.0-0 libnss3 libx11-6 # chromedriver deps
--- crawlab-base/install/golang/golang.sh
+++ crawlab-base/install/golang/golang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -OL https://golang.org/dl/go1.16.7.linux-amd64.tar.gz \
-	&& tar -C /usr/local -xvf go1.16.7.linux-amd64.tar.gz \
+curl -OL https://golang.org/dl/go1.22.4.linux-amd64.tar.gz \
+	&& tar -C /usr/local -xvf go1.22.4.linux-amd64.tar.gz \
 	&& ln -s /usr/local/go/bin/go /usr/local/bin/go
